be4b01f871d351db83a337266db84335420b09aa
Patrick Dolan <dolanp1992@gmail.com>
2024-06-20 13:49:54 -0700

    add color and spacing values

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': '#635FC7',
+        'primary-light': '#A8A4FF',
+        'secondary': '#EA5555',
+        'secondary-light': '#FF9898',
+        'black-1': '#000112',
+        'black-2': '#20212C',
+        'black-3': '#2B2C37',
+        'black-4': '#3E3F4E',
+        'gray': '#828FA3',
+        'light-gray': '#E4EBFA',
+        'off-white': '#F4F7FD',
+      },
       fontFamily: {
         sans: ['"Plus Jakarta Sans"', 'sans-serif'],
       },
@@ -22,6 +35,9 @@ export default {
       letterSpacing: {
         '2.4px': '0.15rem', // 2.4px @ 16px
       },
+      spacing: {
+        '3.5': '0.875rem', // 14px @ 16px
+      }
     },
     plugins: [],
   }
