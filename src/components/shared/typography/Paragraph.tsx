7a6a3feeda78b07b16c5bae1a6cf21bc00819a7f
Patrick Dolan <dolanp1992@gmail.com>
2024-06-26 11:56:37 -0700

    add className prop

--- src/components/shared/typography/Paragraph.tsx
+++ src/components/shared/typography/Paragraph.tsx
@@ -1,14 +1,20 @@
-function Paragraph({ type, text }: { type: string; text: string }) {
-  let classes = '';
+interface ParagraphProps {
+  type: string;
+  text: string;
+  className?: string;
+}
+
+function Paragraph({ type, text, className = '' }: ParagraphProps) {
+  let classes = className;
   switch (type) {
     case 'lg':
-      classes += 'text-13px font-medium leading-5.75';
+      classes += ' text-13px font-medium leading-5.75';
       break;
     case 'md':
-      classes += 'text-xs font-bold leading-3.75';
+      classes += ' text-xs font-bold leading-3.75';
       break;
     default:
-      classes += 'text-13px font-medium leading-5.75';
+      classes += ' text-13px font-medium leading-5.75';
       break;
   }
   return <p className={classes}>{text}</p>;
